732e1c217ac62d014d2abbb412f0a52297c3512a
shamim888 <shamimnw99@gmail.com>
2025-05-01 22:32:19 +0600

    fixed modal

--- index.php
+++ index.php
@@ -13,29 +13,28 @@
             <div class="row">
                 <h2 class="text-primary text-center fs-3 fw-bold py-5">A Easy Todo List</h2>
                 <div class="d-flex justify-content-between align-items-center px-0 py-4">
-                    <button type="button" class=" btn btn-success" data-toggle="modal" data-target="#exampleModal">Add Task</button>
+                    <button type="button" class=" btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Task</button>
                     <button type="button" class=" btn btn-light"><span ><i data-feather="printer" style="width:18px; height:18px; margin-right:8px;"></i></span>Print</button>
                 </div>
                 <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             ...
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                         </div>
                     </div>
                 </div>
+
                 <div class="card shadow-sm">
                     <div class="card-body">
                     <table class="table table-hover">
