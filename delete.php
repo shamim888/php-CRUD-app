71d45c864b7f54ead11db91865ac07796dae739f
shamim888 <shamimnw99@gmail.com>
2025-05-04 00:06:58 +0600

    delete logic added

--- delete.php
+++ delete.php
@@ -1,7 +1,15 @@
 <?php 
 
+include 'db.php';
+
 $id = $_GET['id'];
 
-echo $id;
+$sql = "delete from tasks where id = '$id'";
+
+$val = $db->query($sql);
+
+if($val) {
+    header('location: index.php');
+};
 
 ?>
